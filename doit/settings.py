3d5710e4c2110d2163341ad3dd454c8a2bc9e766
Viiiic207 <vgarcianunez71@gmail.com>
2025-06-16 21:47:17 -0500

    Se añadio la opcion de recuperar contraseña

--- doit/settings.py
+++ doit/settings.py
@@ -118,3 +118,11 @@ LOGOUT_REDIRECT_URL = 'home'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'm3069364@gmail.com'
+EMAIL_HOST_PASSWORD = 'mrgx zhfm wwet lvyv'
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
--- doit_app/urls.py
+++ doit_app/urls.py
@@ -3,6 +3,7 @@
 from django.urls import path
 # from django.urls import path # Esta línea está duplicada, puedes borrar una
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -10,6 +11,10 @@ urlpatterns = [
     path('logout/', views.user_logout_view, name='logout'),
     path('login_experto/', views.login_experto, name='login_experto'),
     path('login_admin/', views.login_admin, name='login_admin'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
     path('principal/', views.principal, name='principal'),
     path('registrarse/', views.registrarse, name='registrarse'),
     path('regisexperto/', views.regisexperto, name='regisexperto'),
